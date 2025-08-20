a09d4ff86b89b5af1b6306bb18f769511cc2708f
Jelle-Rijk <jellerijk@gmail.com>
2025-08-20 06:23:41 +0000

    Added JavaFX

--- src/main/java/com/jellerijk/aoc/aoc2017/App.java
+++ src/main/java/com/jellerijk/aoc/aoc2017/App.java
@@ -1,8 +1,17 @@
 package com.jellerijk.aoc.aoc2017;
 
-public class Startup {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
 	public static void main(String[] args) {
-		
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
 	}
 
 }
