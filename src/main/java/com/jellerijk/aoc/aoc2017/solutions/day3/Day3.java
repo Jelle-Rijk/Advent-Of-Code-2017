b1c663fa2ea6e0ae7746d05c967ee538d1646dd4
Jelle-Rijk <jellerijk@gmail.com>
2025-08-20 15:22:10 +0000

    Maven compilation files

---
+++ src/main/java/com/jellerijk/aoc/aoc2017/solutions/day3/Day3.java
@@ -0,0 +1,17 @@
+package com.jellerijk.aoc.aoc2017.solutions.day3;
+
+import com.jellerijk.aoc.aoc2017.launcher.domain.solution.impl.SolutionImpl;
+
+public class Day3 extends SolutionImpl {
+
+	public Day3() {
+		super(3, "Spiral Memory");
+	}
+
+	@Override
+	protected void initGUI() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
